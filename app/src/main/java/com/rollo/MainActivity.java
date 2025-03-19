b857e1423bb48df6166773e90d0930789af8edc9
jesse <jessemaeko@gmail.com>
2025-03-19 00:20:29 -0600

    Worked on adding buttons to main menu and adding an animation of the background

--- app/src/main/java/com/rollo/MainActivity.java
+++ app/src/main/java/com/rollo/MainActivity.java
@@ -1,6 +1,9 @@
 package com.rollo;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +18,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        //Gets the background From image view
+        ImageView marbleBackground = findViewById(R.id.imageView4);
+
+        Animation waveAnimation = AnimationUtils.loadAnimation(this, R.anim.wave_animation);
+
+        marbleBackground.startAnimation(waveAnimation);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
