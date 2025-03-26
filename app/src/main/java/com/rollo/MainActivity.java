9c0fbb70a4d18c9505145aa25038fa493ac13388
nekhoros <nekhoros@ualberta.ca>
2025-03-25 22:53:44 -0600

    + Changed only a name of a button

--- app/src/main/java/com/rollo/MainActivity.java
+++ app/src/main/java/com/rollo/MainActivity.java
@@ -1,8 +1,11 @@
 package com.rollo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
@@ -31,5 +34,11 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+    }
+    public void goToActivity2 (View view){
+        Intent intent = new Intent (this, GameField.class);
+        startActivity(intent);
     }
 }
