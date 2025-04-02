4ce880e8f65bdd974cc26388b493c1e3f3041ece
brettsiemens <bsiemens@ualberta.ca>
2025-04-02 12:35:00 -0600

    +Created new classes,HandType and HandTypeManager.

---
+++ app/src/main/java/com/rollo/HandTypeManager.java
@@ -0,0 +1,33 @@
+package com.rollo;
+
+import com.rollo.HandType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HandTypeManager {
+    private List<HandType> handTypes = new ArrayList<>();
+
+    public HandTypeManager() {
+        handTypes.add(new HandType("High Die", 5, 1, 1));
+        handTypes.add(new HandType("Pair", 10, 2, 1));
+        handTypes.add(new HandType("Two Pair", 20, 2, 1));
+        handTypes.add(new HandType("Three of a Kind", 20, 3, 1));
+        handTypes.add(new HandType("Small Straight", 30, 3, 1));
+        handTypes.add(new HandType("Large Straight", 40, 4, 1));
+        handTypes.add(new HandType("Full House", 25, 3, 1));
+        handTypes.add(new HandType("Four of a Kind", 30, 4, 1));
+        handTypes.add(new HandType("Yahtzee", 50, 5, 1));
+    }
+
+    public HandType getHandByName(String name) {
+        return handTypes.stream()
+                .filter(h -> h.getName().equalsIgnoreCase(name))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public List<HandType> getAllHands() {
+        return handTypes;
+    }
+}
---
+++ app/src/main/java/com/rollo/Upgrade.java
@@ -0,0 +1,6 @@
+package com.rollo;
+
+public class Upgrade {
+    private String name;
+    private String description;
+}
