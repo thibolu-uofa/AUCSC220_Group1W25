ad533894dda23a0dc01c5b56e707476c162766d8
jesse <jessemaeko@gmail.com>
2025-04-07 10:02:19 -0600

    Worked on a Shop page and started ading functionality to the buttons to go to different pages for the player to be able to pick between two options in each categorie

--- app/src/main/java/com/rollo/Shop.java
+++ app/src/main/java/com/rollo/Shop.java
@@ -1,5 +1,25 @@
 package com.rollo;
 
-public class Shop {
-    private int
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Shop extends AppCompatActivity {
+    private int money;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        String layoutName = getIntent().getStringExtra("layoutName");
+
+        if (layoutName != null){
+            int layoutId = getResources().getIdentifier(layoutName, "layout", getPackageName());
+            if (layoutId != 0){
+                setContentView(layoutId);
+            }
+        }
+    }
 }
--- app/src/main/java/com/rollo/ShopPage.java
+++ app/src/main/java/com/rollo/ShopPage.java
@@ -1,8 +1,10 @@
 package com.rollo;
 
 import android.animation.ObjectAnimator;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ShopPage extends AppCompatActivity {
@@ -19,6 +21,29 @@ public class ShopPage extends AppCompatActivity {
         animator.setRepeatCount(ObjectAnimator.INFINITE);
         animator.setRepeatMode(ObjectAnimator.RESTART);
         animator.start();
+
+    }
+
+    public void onUpgradeClick(View view){
+        openShopPage("shop_upgrade");
+    }
+
+    public void onCombosClick(View view){
+        openShopPage("shop_combos");
+    }
+
+    public void onPaintingClick(View view){
+        openShopPage("shop_painting");
+    }
+
+    public void onVouchersClick(View view){
+        openShopPage("shop_vouchers");
+    }
+
+    private void openShopPage(String layoutName){
+        Intent intent = new Intent(ShopPage.this, Shop.class);
+        intent.putExtra("layoutName", layoutName);
+        startActivity(intent);
     }
 
 }
