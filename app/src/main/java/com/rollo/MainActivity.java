cf1e74ff2f83acc3f921001361debae887771867
nekhoros <nekhoros@ualberta.ca>
2025-04-07 09:16:39 -0600

    + Got rid of an unused library

--- app/src/main/java/com/rollo/MainActivity.java
+++ app/src/main/java/com/rollo/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
