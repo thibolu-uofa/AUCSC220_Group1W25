5d5f657f96d9fce243bd3aca7cc22d83de4a562f
timi-a <timiaina10@gmail.com>
2025-04-07 01:08:26 -0600

    Finalized all menu event listeners.

--- app/src/main/java/com/rollo/EventListener.java
+++ app/src/main/java/com/rollo/EventListener.java
@@ -1,42 +1,122 @@
 package com.rollo;
 
-import android.app.Activity;
-import android.view.MotionEvent;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
+import android.view.animation.Animation;
 import android.widget.Button;
 
-import androidx.core.content.ContextCompat;
+import java.util.Arrays;
+import java.util.List;
 
 public class EventListener {
+    private final MainActivity mainActivity;
+    private List<Button> mainButtons;
 
-    public EventListener(Activity activity) {
-        // Initialize buttons by finding them in the activity layout
-        Button loadGameButton = activity.findViewById(R.id.loadGameButton);
-        Button newGameButton = activity.findViewById(R.id.newGameButton);
-        Button tutorialButton = activity.findViewById(R.id.tutorialButton);
-        Button creditsButton = activity.findViewById(R.id.creditsButton);
+    private View dimOverlay;
 
-        // Initialize main menu screen views
-        View tutorialBackground = activity.findViewById(R.id.tutorialBackground);
-        View creditsBackground = activity.findViewById(R.id.creditsBackground);
-        Button creditsBackButton = activity.findViewById(R.id.creditsBackButton);
+    //Transition Views
+    private View blackoutView;
+    //private final View diceView;
+    private final AnimationPlayer animPlayer;
 
-        // Set click/tap listeners for main menu buttons
-        loadGameButton.setOnClickListener(v -> {});
-        newGameButton.setOnClickListener(v -> {});
-        tutorialButton.setOnClickListener(v -> {
-            tutorialBackground.setVisibility(View.VISIBLE);
-        });
+    private final Handler handler = new Handler(Looper.getMainLooper());
 
-        creditsButton.setOnClickListener(v -> {
-            creditsBackground.setVisibility(View.VISIBLE);
-            creditsBackButton.setVisibility(View.VISIBLE);
-        });
+    private TouchColorButton loadGameButton, newGameButton, tutorialButton, creditsButton;
+    private TouchColorButton tutorialBackButton, creditsBackButton;
+    private View tutorialView, creditsView;
 
-        creditsBackButton.setOnClickListener(v->{
-            creditsBackground.setVisibility(View.GONE);
-            creditsBackButton.setVisibility(View.GONE);
-        });
-    }
-}
+    public EventListener(MainActivity mainActivity) {
+        this.mainActivity = mainActivity;
+        this.animPlayer = new AnimationPlayer(mainActivity);
 
+        initViews();
+        setUpButtons();
+        setUpListeners();
+    }//EventListener
+
+    private void initViews() {
+        dimOverlay = mainActivity.findViewById(R.id.dimOverlay);
+        blackoutView = mainActivity.findViewById(R.id.blackoutView);
+        //diceView = mainActivity.findViewById(R.id.diceView);
+
+        loadGameButton = mainActivity.findViewById(R.id.loadGameButton);
+        newGameButton = mainActivity.findViewById(R.id.newGameButton);
+        tutorialButton = mainActivity.findViewById(R.id.tutorialButton);
+        creditsButton = mainActivity.findViewById(R.id.creditsButton);
+
+        tutorialBackButton = mainActivity.findViewById(R.id.tutorialBackButton);
+        creditsBackButton = mainActivity.findViewById(R.id.creditsBackButton);
+        tutorialView = mainActivity.findViewById(R.id.tutorialView);
+        creditsView = mainActivity.findViewById(R.id.creditsView);
+    }//initViews
+
+    // Store all core buttons in a list for easy disabling/enabling
+    private void setUpButtons(){
+        mainButtons = Arrays.asList(loadGameButton, newGameButton, tutorialButton, creditsButton);
+    }//setUpButtons
+
+    private void setUpListeners(){
+        loadGameButton.setOnClickListener(v -> playGameTransition(animPlayer.radialBlackout(),
+                animPlayer.diceRotate()));
+        newGameButton.setOnClickListener(v -> playGameTransition(animPlayer.radialBlackout(),
+                animPlayer.diceRotate()));
+        tutorialButton.setOnClickListener(v -> showOverlay(tutorialView, tutorialBackButton,
+                animPlayer.slideUp()));
+        creditsButton.setOnClickListener(v -> showOverlay(creditsView, creditsBackButton,
+                animPlayer.slideUp()));
+        tutorialBackButton.setOnClickListener(v -> hideOverlay(tutorialView, tutorialBackButton,
+                animPlayer.slideDown()));
+        creditsBackButton.setOnClickListener(v -> hideOverlay(creditsView, creditsBackButton,
+                animPlayer.slideDown()));
+    }//setUpListeners
+
+    private void enableButtons(boolean enabled) {
+        for (Button button : mainButtons) {
+            button.setEnabled(enabled);
+        }//for-loop
+    }//enableButtons
+
+    private void showOverlay(View overlayView, Button backButton, Animation slideUp) {
+        overlayView.setVisibility(View.VISIBLE);
+        overlayView.startAnimation(slideUp);
+
+        backButton.setVisibility(View.VISIBLE);
+        backButton.startAnimation(slideUp);
+
+        dimOverlay.setVisibility(View.VISIBLE);
+        dimOverlay.startAnimation(animPlayer.fadeIn());
+
+        enableButtons(false);
+    }//showOverlay
+
+    private void hideOverlay(View overlayView, Button backButton, Animation slideDown) {
+
+        overlayView.startAnimation(slideDown);
+        handler.postDelayed(() -> overlayView.setVisibility(View.GONE), slideDown.getDuration());
+
+        backButton.startAnimation(slideDown);
+        handler.postDelayed(() -> backButton.setVisibility(View.GONE), slideDown.getDuration());
+
+        dimOverlay.startAnimation(animPlayer.fadeOut());
+        handler.postDelayed(() -> dimOverlay.setVisibility(View.GONE),
+                animPlayer.fadeOutAnim.getDuration());
+
+        enableButtons(true);
+    }//hideOverlay
+
+
+    private void playGameTransition(Animation radialBlackout, Animation diceRotate) {
+            blackoutView.startAnimation(radialBlackout);
+
+            //diceView.setVisibility(View.VISIBLE);
+            //diceView.startAnimation(diceRotationAnim);
+
+            long blackoutDuration = radialBlackout.getDuration();
+            long rotateDuration = diceRotate.getDuration();
+            long animDuration = Math.max(blackoutDuration, rotateDuration);
+
+            handler.postDelayed(() -> MainActivity.moveToGame(mainActivity), animDuration);
+    }//playGameTransition
+
+}//EventListener
--- app/src/main/java/com/rollo/MainActivity.java
+++ app/src/main/java/com/rollo/MainActivity.java
@@ -1,17 +1,15 @@
 package com.rollo;
 
-import android.graphics.Color;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -27,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         //Gets the background From image view
         ImageView gameBackground = findViewById(R.id.background);
 
-        Animation waveAnimation = AnimationUtils.loadAnimation(this, R.anim.wave_animation);
+        Animation waveAnimation = AnimationUtils.loadAnimation(this, R.anim.wave);
 
         gameBackground.startAnimation(waveAnimation);
 
@@ -38,4 +36,13 @@ public class MainActivity extends AppCompatActivity {
         });
         new EventListener(this);
     }
+
+    protected static void moveToGame(Context context) {
+        Intent intent = new Intent(context, GameField.class);
+        context.startActivity(intent);
+
+        if (context instanceof Activity) {
+            ((Activity) context).finish(); // Close current activity
+        }//if statement
+    }//moveToGame
 }
