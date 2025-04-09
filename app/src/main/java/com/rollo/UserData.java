2a77b973e2a8a20bcef96924f0db1f6d930cd6ac
nekhoros <nekhoros@ualberta.ca>
2025-04-09 09:15:14 -0600

    + Added ways to access new info from the json file

--- app/src/main/java/com/rollo/UserData.java
+++ app/src/main/java/com/rollo/UserData.java
@@ -23,13 +23,17 @@ class GameState {
     private int money;
     private int rerolls;
     private int plays;
+    private int currentScore;
+    private int scoreToBeat;
+    private int maxRound;
 
     public GameState(){
         this.round = 0;
         this.highScore = 0;
         this.money = 0;
-        this.rerolls = 0;
-        this.plays = 3;
+        this.plays = 4;
+        this.rerolls = 5;
+        this.maxRound = 20;
     }
 
     public int getHighScore() {return highScore;}
@@ -37,11 +41,18 @@ class GameState {
     public int getPlays() {return plays;}
     public int getRerolls() {return rerolls;}
     public int getRound() {return round;}
+    public int getCurrentScore() {return currentScore;}
+    public int getScoreToBeat() {return scoreToBeat;}
+
+    public int getMaxRound() {return maxRound;}
 
     public void setHighScore(int highScore) {this.highScore = highScore;}
     public void setMoney(int money) {this.money = money;}
     public void setPlays(int plays) {this.plays = plays;}
-
     public void setRerolls(int rerolls) {this.rerolls = rerolls;}
     public void setRound(int round) {this.round = round;}
+    public void setCurrentScore(int newScore) {this.currentScore = newScore;}
+    public void setScoreToBeat(int scoreToBeat) {this.scoreToBeat = scoreToBeat;}
+
+    public void setMaxRound(int maxRound) {this.maxRound = maxRound;}
 }
