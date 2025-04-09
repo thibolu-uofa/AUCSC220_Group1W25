6234224f5d3e5b1c22d4e8c1c43dd1c743841864
nekhoros <nekhoros@ualberta.ca>
2025-04-08 20:51:57 -0600

    + Removed finality from string name to give a change to set it

--- app/src/main/java/com/rollo/HandType.java
+++ app/src/main/java/com/rollo/HandType.java
@@ -1,10 +1,12 @@
 package com.rollo;
 
 public class HandType {
-    private final String name;
+    private String name;
     private int pips;
     private int mult;
     private int level;
+
+    public HandType(){}
 
     public HandType(String name, int pips, int mult, int level) {
         this.name = name;
@@ -18,6 +20,7 @@ public class HandType {
     public int getMult() { return mult; }
     public int getLevel() { return level; }
 
+    public void setName(String name) { this.name = name; }
     public void setPips(int pips) { this.pips = pips; }
     public void setMult(int mult) { this.mult = mult; }
     public void setLevel(int level) { this.level = level; }
