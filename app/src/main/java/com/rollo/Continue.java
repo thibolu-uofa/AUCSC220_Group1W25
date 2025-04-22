7d3c142cfc614f49e2543014f0a7fd8abcb10a3e
nekhoros <nekhoros@ualberta.ca>
2025-04-21 21:44:58 -0600

    + Added Documentation to Continue.java

--- app/src/main/java/com/rollo/Continue.java
+++ app/src/main/java/com/rollo/Continue.java
@@ -1,3 +1,45 @@
+/**
+ * Continue.java
+ *
+ * Author: Miron Nekhoroshkov
+ *
+ * This file gives way to a user to save their data, and developers
+ * to more easily implement inter file communication. This file
+ * is made with the use of Google's json reader library.
+ *
+ * functions:
+ *      getHandTypesFromJson()
+ *          gets the handtypes saved in the userdata.json file
+ *      getDiceFromJson()
+ *          gets the dice saved in the userdata.json file
+ *      getRerollFromJson()
+ *          gets the amount of rerolls saved in the userdata.json file
+ *      getPlaysFromJson()
+ *          gets the amount of plays saved in the userdata.json file
+ *      getScoreToBeatFromJson()
+ *          gets the needed score to beat saved in the userdata.json file
+ *      getCurrentScoreFromJson()
+ *          gets the current score saved in the userdata.json file
+ *      copyJsonToInternalStorageIfNeeded(Context context)
+ *          saves the json file to internal storage for save state to work
+ *      setCurrentScore(Context context, int amount)
+ *          saves the current score to the json file
+ *      setScoreToBeat(Context context, int amount)
+ *          saves the score to beat to the json file
+ *      setRounds(Context context, int amount)
+ *          saves the rounds the user played to the json file
+ *      setHighScore(Context context)
+ *          saves the highscore the user got to the json file
+ *      setPlays(Context context, int amount)
+ *          saves the amount of plays to the json file
+ *      setRerolls(Context context, int amount)
+ *          saves the amount of Rerolls to the json file
+ *      setHandtypes(Context context, List<HandType> handTypes)
+ *          saves the hands to the json file
+ *      resetToDefaults(Context context)
+ *          resets the userdata.json file
+ */
+
 package com.rollo;
 
 import android.content.Context;
@@ -17,11 +59,19 @@ public class Continue {
 
     private static final String FILENAME = "userdata.json";
 
+    //Basic Constructor
     public Continue(Context context) {
         this.context = context;
-        InputStream inputStream = context.getResources().openRawResource(R.raw.userdata);
     }
 
+    /**
+     * getHandTypesFromJson
+     *
+     * using the google Json reader, it will use the UserData.java
+     * class to cleanse the file "userdata.json" and output the proper data
+     *
+     * @return all the handtypes saved in the json file
+     */
     public List<HandType> getHandTypesFromJson() {
         try {
             File file = new File(context.getFilesDir(), FILENAME);
@@ -39,6 +89,14 @@ public class Continue {
         }
     }
 
+    /**
+     * getDiceFromJson
+     *
+     * using the google Json reader, it will use the UserData.java
+     * class to cleanse the file "userdata.json" and output the proper data
+     *
+     * @return all the dice saved in the json file
+     */
     public Dice[] getDiceFromJson() {
         try {
             File file = new File(context.getFilesDir(), FILENAME);
@@ -56,6 +114,14 @@ public class Continue {
         }
     }
 
+    /**
+     * getRerollFromJson
+     *
+     * using the google Json reader, it will use the UserData.java
+     * class to cleanse the file "userdata.json" and output the proper data
+     *
+     * @return the number of rerolls allowed saved in the json file
+     */
     public int getRerollFromJson() {
         try {
             File file = new File(context.getFilesDir(), FILENAME);
@@ -73,6 +139,14 @@ public class Continue {
         }
     }
 
+    /**
+     * getPlaysFromJson
+     *
+     * using the google Json reader, it will use the UserData.java
+     * class to cleanse the file "userdata.json" and output the proper data
+     *
+     * @return the number of Plays allowed saved in the json file
+     */
     public int getPlaysFromJson() {
         try {
             File file = new File(context.getFilesDir(), FILENAME);
@@ -90,6 +164,14 @@ public class Continue {
         }
     }
 
+    /**
+     * getScoreToBeatFromJson
+     *
+     * using the google Json reader, it will use the UserData.java
+     * class to cleanse the file "userdata.json" and output the proper data
+     *
+     * @return the score to beat saved in the json file
+     */
     public int getScoreToBeatFromJson() {
         try {
             File file = new File(context.getFilesDir(), FILENAME);
@@ -106,6 +188,15 @@ public class Continue {
             return 0;
         }
     }
+
+    /**
+     * getCurrentScoreFromJson
+     *
+     * using the google Json reader, it will use the UserData.java
+     * class to cleanse the file "userdata.json" and output the proper data
+     *
+     * @return the current score saved in the json file
+     */
     public int getCurrentScoreFromJson() {
         try {
             File file = new File(context.getFilesDir(), FILENAME);
@@ -123,6 +214,15 @@ public class Continue {
         }
     }
 
+    /**
+     * copyJsonToInternalStorageIfNeeded
+     *
+     * instead having the userdata.json reset everytime the user
+     * starts the app, the userdata.json is written to internal storage
+     * and is dugout again to be read.
+     *
+     * @param context
+     */
     public void copyJsonToInternalStorageIfNeeded(Context context) {
         File file = new File(context.getFilesDir(), "userdata.json");
         if (!file.exists()) {
@@ -139,6 +239,14 @@ public class Continue {
         }
     }
 
+    /**
+     * setCurrentScore
+     *
+     * sets the current score into the internal storage
+     *
+     * @param context
+     * @param amount
+     */
     public void setCurrentScore(Context context, int amount) {
         try {
             File file = new File(context.getFilesDir(), FILENAME);
@@ -166,6 +274,14 @@ public class Continue {
         }
     }
 
+    /**
+     * setScoreToBeat
+     *
+     * sets the score to beat into the internal storage
+     *
+     * @param context
+     * @param amount
+     */
     public void setScoreToBeat(Context context, int amount) {
         try {
             File file = new File(context.getFilesDir(), FILENAME);
@@ -193,6 +309,14 @@ public class Continue {
         }
     }
 
+    /**
+     * setRounds
+     *
+     * sets the round a certain user has reached into the internal storage
+     *
+     * @param context
+     * @param amount
+     */
     public void setRounds(Context context, int amount) {
         try {
             File file = new File(context.getFilesDir(), FILENAME);
@@ -220,6 +344,15 @@ public class Continue {
         }
     }
 
+    /**
+     * setHighScore
+     *
+     * sets the high score a user reached into the internal storage,
+     * this is done by simply calling the function with one parameter
+     * as the check will happen within the function
+     *
+     * @param context
+     */
     public void setHighScore(Context context) {
         try {
             File file = new File(context.getFilesDir(), FILENAME);
@@ -249,6 +382,14 @@ public class Continue {
         }
     }
 
+    /**
+     * setPlays
+     *
+     * sets the plays allowed by one user into the internal storage
+     *
+     * @param context
+     * @param amount
+     */
     public void setPlays(Context context, int amount) {
         try {
             File file = new File(context.getFilesDir(), FILENAME);
@@ -278,6 +419,14 @@ public class Continue {
         }
     }
 
+    /**
+     * setRerolls
+     *
+     * sets the reroll amount a user is allowed into the internal storage
+     *
+     * @param context
+     * @param amount
+     */
     public void setRerolls(Context context, int amount) {
         try {
             File file = new File(context.getFilesDir(), FILENAME);
@@ -297,15 +446,24 @@ public class Continue {
                 // Write it back
                 try (FileWriter writer = new FileWriter(file)) {
                     gson.toJson(data, writer);
-                    Log.d("CONTINUE", "High score is: " + data.getGameState().getCurrentScore());
+                    Log.d("CONTINUE", "Amount of rerolls is: " + data.getGameState().getRerolls());
                 }
             }
 
         } catch (Exception e) {
-            Log.e("CONTINUE", "Failed to setCurrentScore", e);
+            Log.e("CONTINUE", "Failed to setRerolls", e);
         }
     }
 
+    /**
+     * setHandTypes
+     *
+     * sets the current scoring and level of each hand from a user
+     * into the internal storage
+     *
+     * @param context
+     * @param handTypes
+     */
     public void setHandtypes(Context context, List<HandType> handTypes) {
         try {
             File file = new File(context.getFilesDir(), FILENAME);
@@ -334,6 +492,13 @@ public class Continue {
         }
     }
 
+    /**
+     * resetToDefaults
+     *
+     * will reset all the information gained in a run of ROLLO
+     *
+     * @param context
+     */
     public void resetToDefaults(Context context) {
         File file = new File(context.getFilesDir(), FILENAME);
 
