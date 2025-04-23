11a87c9a13c3d29b285589aec03341377cde3ea3
brettsiemens <bsiemens@ualberta.ca>
2025-04-22 20:40:01 -0600

    + Added Documentation
    Provided documentation for Upgrade.java

--- app/src/main/java/com/rollo/Upgrade.java
+++ app/src/main/java/com/rollo/Upgrade.java
@@ -1,3 +1,12 @@
+/**
+ *
+ * Upgrade.java is an umbrella class inherited by Combo and Voucher.
+ * Currently unused, is meant to give each type of upgrade a name and description
+ * attribute.
+ *
+ * @author - Brett Siemens
+ */
+
 package com.rollo;
 
 public class Upgrade {
