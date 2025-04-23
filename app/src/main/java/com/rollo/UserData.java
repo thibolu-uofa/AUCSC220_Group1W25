3d5a04d244b21cb32285d065028ecc5c1c027e4e
nekhoros <nekhoros@ualberta.ca>
2025-04-22 22:20:52 -0600

    + Made a way to save scoring

--- app/src/main/java/com/rollo/UserData.java
+++ app/src/main/java/com/rollo/UserData.java
@@ -42,40 +42,36 @@ public class UserData {
 }
 class GameState {
     private int round;
-    private int highScore;
-    private int money;
+    private int runHighScore;
     private int rerolls;
     private int plays;
     private int currentScore;
     private int scoreToBeat;
-    private int maxRound;
+    private int allTimeHighScore;
 
     public GameState(){
         this.round = 0;
-        this.highScore = 0;
-        this.money = 0;
+        this.runHighScore = 0;
         this.plays = 4;
         this.rerolls = 5;
-        this.maxRound = 20;
+        this.allTimeHighScore = 0;
     }
 
-    public int getHighScore() {return highScore;}
-    public int getMoney() {return money;}
+    public int getRunHighScore() {return runHighScore;}
+    public int getAllTimeHighScore() {return allTimeHighScore;}
     public int getPlays() {return plays;}
     public int getRerolls() {return rerolls;}
     public int getRound() {return round;}
     public int getCurrentScore() {return currentScore;}
     public int getScoreToBeat() {return scoreToBeat;}
 
-    public int getMaxRound() {return maxRound;}
-
-    public void setHighScore(int highScore) {this.highScore = highScore;}
-    public void setMoney(int money) {this.money = money;}
+    public void setRunHighScore(int highScore) {this.runHighScore = highScore;}
+    public void setAllTimeHighScore(int allTimeHighScore)
+        {this.allTimeHighScore = allTimeHighScore;}
     public void setPlays(int plays) {this.plays = plays;}
     public void setRerolls(int rerolls) {this.rerolls = rerolls;}
     public void setRound(int round) {this.round = round;}
     public void setCurrentScore(int newScore) {this.currentScore = newScore;}
     public void setScoreToBeat(int scoreToBeat) {this.scoreToBeat = scoreToBeat;}
 
-    public void setMaxRound(int maxRound) {this.maxRound = maxRound;}
 }
