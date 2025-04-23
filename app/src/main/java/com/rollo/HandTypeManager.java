99ce1797e8bf2493d8e22d0e8abd63e93fe5d7df
brettsiemens <bsiemens@ualberta.ca>
2025-04-22 21:28:48 -0600

    + Improved documentation
    Improved documentation of HandTypeManager.java

--- app/src/main/java/com/rollo/HandTypeManager.java
+++ app/src/main/java/com/rollo/HandTypeManager.java
@@ -26,6 +26,9 @@ import java.util.List;
 public class HandTypeManager {
     private List<HandType> handTypes = new ArrayList<>();
 
+    /**
+     * Initializes nine array types on empty constructor
+     */
     public HandTypeManager() {
         handTypes.add(new HandType("High Die", 5, 1, 1));
         handTypes.add(new HandType("Pair", 10, 2, 1));
@@ -38,10 +41,19 @@ public class HandTypeManager {
         handTypes.add(new HandType("Yahtzee", 50, 5, 1));
     }
 
+    /**
+     * Uses previously initialized hand types on constructor with a parameter
+     * @param handTypes
+     */
     public HandTypeManager(List<HandType> handTypes){
         this.handTypes = handTypes;
     }
 
+    /**
+     * Gets a hand type from a String
+     * @param name - String representing a hand type
+     * @return HandType object of that hand
+     */
     public HandType getHandByName(String name) {
         for (HandType hand : handTypes) {
             if (hand.getName().equals(name)) {
@@ -55,10 +67,19 @@ public class HandTypeManager {
         this.handTypes = handTypes;
     }
 
+    /**
+     * Get list of all hand types
+     * @return - List of hand types
+     */
     public List<HandType> getAllHands() {
         return handTypes;
     }
 
+    /**
+     * Uses Combo class to upgrade hand type
+     * @param handName - old hand type to be upgraded
+     * @param newHandtype - upgraded hand type to be set in place of old one
+     */
     public void upgradeHand(String handName, HandType newHandtype) {
         for (int i = 0; i < handTypes.size(); i++) {
             if (handTypes.get(i).getName().equalsIgnoreCase(handName)) {
